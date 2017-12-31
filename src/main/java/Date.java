89ef6ddaa620e87b1604b538b7405b33aeac1773
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-12-31 11:08:12 +0200

    add classes and delete methods

--- src/main/java/Date.java
+++ src/main/java/Date.java
@@ -28,9 +28,9 @@ public class Date{
     private int day, month, year;
     private final Random random = new Random();
     public Date(){
-        day = random.nextInt(31);
-        month = random.nextInt(12);
-        year = random.nextInt(2019);
+        day = random.nextInt(31) ;
+        month = random.nextInt(12)  ;
+        year = 2018;
     }
 
     public Date(int day, int month, int year){
@@ -38,4 +38,39 @@ public class Date{
         this.month = month;
         this.year = year;
     }
+
+    public boolean before(Date other){
+        // if dates equal return true
+        if(other.year > year )
+            return true;
+        if (other.year == year){
+            if(other.month > month)
+                return true;
+            if(other.month == month){
+                if(other.day > day){
+                    return true;
+                }
+                if(other.day == day)
+                    return true;
+
+            }
+
+        }
+        return false;
+    }
+    public Date add(int number_days){
+        int da,m,y;
+        da = (day+number_days ) % 30;
+        m = (month + (day+number_days ) / 30)%12;
+        y = year + (month + (day+number_days ) / 30)/12;
+        return new Date(da, m,y);
+    }
+
+
+    @Override
+    public String toString() {
+        return day+1+ "/"+month+1+ "/"+year;
+    }
+
+
 }
--- src/main/java/SkiPassFather.java
+++ src/main/java/SkiPassFather.java
@@ -1,11 +1,9 @@
-import java.util.Calendar;
-
 class SkiPassFather {
-    private KindSkiPass skipass_type ;
+
     private int counter;
     private static int counter_id;
     private int id = counter_id++;
-    private String day_start = "";
+
 
 
     public boolean isBan() {
@@ -14,20 +12,6 @@ class SkiPassFather {
 
     private boolean ban = false;
 
-    public String get_skipass_type(){
-        return skipass_type.skipass_type();
-    }
-
-    public String getDay_start(){
-        return day_start;
-    }
-
-    public int today(){
-        Calendar cal = Calendar.getInstance();
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        return day;
-    }
 
     public void minus_counter(){
             counter--;
@@ -37,16 +21,7 @@ class SkiPassFather {
         return counter>0 && !ban;
     }
 
-    public void make_day_start_today(){
-        minus_counter();
-        //day_start = today();
-    }
 
-    public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        cal.add(Calendar.DAY_OF_MONTH, -3);
-        int dsay1 = cal.get(Calendar.DAY_OF_MONTH);
-        System.out.println(cal.before(cal) +" "+ day);
-    }
+
+
 }
